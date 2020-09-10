1eb88d14e272ee49ab18ac3257c07ec6991c175f
sankark2k <70580228+sankark2k@users.noreply.github.com>
2020-09-10 07:13:03 -0700

    Update day-4-assignment-b7.py

--- day-4-assignment-b7.py
+++ day-4-assignment-b7.py
@@ -2,10 +2,11 @@
 
 for num in range(1042000,702648265):
     sum = 0
+    n=len(str(num))
     temp = num
     while temp>0:
         digit=temp%10
-        sum +=digit**3
+        sum +=digit**n
         temp //=10
     if num == sum:
         print('The first ARMSTRONG number - ',num )
